3dfc63c4d42ee43e97717a5a661b8824083d8c45
Gerco Ballintijn <gerco@ballintijn.com>
2024-06-21 13:57:12 +0200

    Added a basic C++ application with its configuration.

---
+++ application.cpp
@@ -0,0 +1,15 @@
+//
+// application.cpp -- CMakeTemplate
+//
+
+#include <cstdlib>
+#include <iostream>
+
+
+int
+main(int, const char **)
+{
+    std::cout << "Application template" << std::endl;
+
+    return EXIT_SUCCESS;
+}
