69cb9a96ecfd4ac1d353e6f4671f1b809189a46b
Gerco Ballintijn <gerco@ballintijn.com>
2024-06-21 20:09:50 +0200

    Added version injection.

--- application.cpp
+++ application.cpp
@@ -5,11 +5,19 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "config.hpp"
+
 
 int
 main(int, const char **)
 {
-    std::cout << "Application template" << std::endl;
+    std::cout << "Application template" 
+            << " (" << cmake_template_version << ")"
+            << std::endl;
+    std::cout << "major: " << cmake_template_major
+            << " minor: " << cmake_template_minor
+            << " patch: " << cmake_template_patch
+            << std::endl;
 
     return EXIT_SUCCESS;
 }
